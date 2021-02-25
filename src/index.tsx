8584cf91657173f48f51aca2627e78abd2a548f7
Jessica J Sun <jessi.slm@hotmail.com>
2021-02-25 12:05:24 +1100

    Adding Monaco code editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -62,6 +64,10 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -69,7 +75,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title="Code executor" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <iframe title="code editor" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
