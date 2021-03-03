46450071720438b68b03f7140c0a9bd3ec0349e8
Jessica J Sun <jessi.slm@hotmail.com>
2021-03-03 13:07:14 +1100

    Adding markdown editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
 
 const App = () => {
