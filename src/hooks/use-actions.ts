d3dd7c1ddca2e05ce8c7bdf4057fd12fbc3d5661
Jessica J Sun <jessi.slm@hotmail.com>
2021-03-07 09:04:46 +1100

    Revert "Connecting bundles in redux"
    This reverts commit 0b4b52af12d1e78a0988d87446422b37b0e214a0.

--- src/hooks/use-actions.ts
+++ src/hooks/use-actions.ts
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state';
@@ -6,7 +5,5 @@ import { actionCreators } from '../state';
 export const useActions = () => {
   const dispatch = useDispatch();
 
-  return useMemo(() => {
-    return bindActionCreators(actionCreators, dispatch);
-  }, [dispatch]);
+  return bindActionCreators(actionCreators, dispatch);
 };
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import cellsReducer from './cellsReducer';
-import bundlesReducer from './bundlesReducer';
 
 const reducers = combineReducers({
   cells: cellsReducer,
-  bundles: bundlesReducer,
 });
 
 export default reducers;
